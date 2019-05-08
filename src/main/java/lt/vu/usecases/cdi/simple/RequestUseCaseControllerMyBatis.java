b49925f50ae4518d8ca07371ddbb9f9044bcbf7e
Lukas Cedronas <lukascedronas1@gmail.com>
2019-05-08 08:55:12 +0300

    batis some updates

--- src/main/java/lt/vu/usecases/cdi/simple/RequestUseCaseControllerMyBatis.java
+++ src/main/java/lt/vu/usecases/cdi/simple/RequestUseCaseControllerMyBatis.java
@@ -7,38 +7,79 @@ import javax.enterprise.inject.Model;
 @Model // tas pats kaip: @Named ir @RequestScoped
 @Slf4j
 public class RequestUseCaseControllerMyBatis {
-//
+
 //    @Getter
-//    private Course course = new Course();
+//    private Order order = new Order();
 //    @Getter
-//    private Student student = new Student();
+//    private Customer customer = new Customer();
 //    @Getter
-//    private List<Student> allStudents;
+//    private Product product = new Product();
+//    @Getter
+//    private List<Order> allOrders;
+//    @Getter
+//    private List<Product> allProducts;
 //
 //    @PostConstruct
 //    public void init() {
-//        loadAllStudents();
+//        loadAllOrders();
+//        loadAllProducts();
 //    }
 //
 //    @Inject
-//    private StudentMapper studentMapper;
+//    private ProductCategoryMapper productCategoryDao;
 //    @Inject
-//    private CourseMapper courseMapper;
+//    private ProductMapper productDao;
 //    @Inject
-//    private StudentCourseMapper studentCourseMapper;
+//    private OrderMapper orderDao;
+//    @Inject
+//    private CustomerMapper customerDao;
+//
 //
 //    @Transactional
-//    public void createCourseStudent() {
-//        courseMapper.insert(course);
-//        studentMapper.insert(student);
-//        StudentCourse studentCourse = new StudentCourse();
-//        studentCourse.setCourseId(course.getId());
-//        studentCourse.setStudentId(student.getId());
-//        studentCourseMapper.insert(studentCourse);
+//    public void createOrder() {
+//        Product existingProduct = productDao.selectAll().stream()
+//                .filter(p -> p.getName() == product.getName())
+//                .collect(Collectors.toList()).get(0);
+//
+//        int productCategoryId = existingProduct.getProductCategoryId();
+//        List<Integer> productCategories = new ArrayList<>();
+//
+//
+//        productCategories.add(productCategoryId);
+//
+//        productCategoryDao.selectAll().get(0)
+//
+//        order.setProductCategoryList(productCategories);
+//        Customer customer = customerDao.findByUserName(this.customer.getUserName());
+//        order.setCustomer(customer);
+//
+//        if (existingProduct == null) {
+//            productDao.create(product);
+//            productCategoryDao.create(productCategory);
+//        }
+//
+//        orderDao.create(order);
+//
 //        log.info("Maybe OK...");
 //    }
 //
-//    private void loadAllStudents() {
-//        allStudents = studentMapper.selectAll();
+//    @Transactional
+//    public void createProduct() {
+//        Product existingProduct = productDao.findByName(product.getName());
+//        productCategory = existingProduct == null ? productCategory : existingProduct.getProductCategory();
+//
+//        product.setProductCategory(productCategory);
+//        productDao.create(product);
+//
+//        log.info("Maybe OK...");
+//    }
+//
+//
+//    private void loadAllOrders() {
+//        allOrders = orderDao.getAllOrders();
+//    }
+//
+//    private void loadAllProducts() {
+//        allProducts = productDao.getAllProducts();
 //    }
 }
